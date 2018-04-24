8dbd6c390abbc2133d43fe80188812560fba3faf
Tina Wu <ywu6@nd.edu>
2018-04-24 19:27:57 -0400

    wikiCrawer.py

--- wikiCrawer.py
+++ wikiCrawer.py
@@ -5,20 +5,44 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-url = input("Enter a link: ")
-source = requests.get(url)
-data = source.text
-soup = BeautifulSoup(data,"html.parser")
-numnode = input("Enter the number of links to be shown: ")
 
-f = open("links.txt","w+")
- 
-for link in soup.find_all('a'):
-	if (link.get('href','').startswith('/wiki/') and not link.get('href','').startswith('/wiki/Special:')):
-		f.write(link.get('href'))
-		f.write('\n')
+def getlinks(url="https://en.wikipedia.org/wiki/Main_Page", filenum = 0):
+	source = requests.get(url)
+	data = source.text
+	soup = BeautifulSoup(data,"html.parser")
+	numnode = input("Enter the number of links to be shown: ")
+	
+	f = open("links.txt","w+")
+	
+	for link in soup.find_all('a'):
+		if (link.get('href','').startswith('/wiki/') and not link.get('href','').startswith('/wiki/Special:')):
+			f.write(link.get('href'))
+			f.write('\n')
 
-commandsort = "cat links.txt | sort | uniq -c | sort -n | tail -" + str(numnode) + " | sort -nr > toplinks.txt"
-os.system(commandsort)
-os.system("rm links.txt")
+	commandsort = "cat links.txt | sort | uniq -c | sort -n | tail -" + str(numnode) + " | sort -nr > toplinks" + str(filenum) + ".txt"
+	os.system(commandsort)
+	os.system("rm links.txt")
+
+
+layers = int(input("Enter layers: "))
+
+url = input("Please enter a wikipedia url: ")
+while not url.startswith("https://en.wikipedia.org"):
+	print("Invalid URL")
+	url = input("Enter a new URL: ")
+
+print(url)
+
+count = 0;
+
+while layers>0:
+	getlinks(url, count)
+	linkstr="toplinks"+str(count)+".txt"
+	for lines in open(linkstr,"r").read().splitlines():
+		count += 1
+		lines = lines.split()
+		newurl = "https://en.wikipedia.org" + lines[1]
+		getlinks(newurl, count)
+	layers -= 1
+
 
