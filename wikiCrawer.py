dad35342872d68720739580659b9fae29dc7f1e5
Tina Wu <ywu6@nd.edu>
2018-04-25 19:28:34 -0400

    wikiCrawer.py

--- wikiCrawer.py
+++ wikiCrawer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 import sys 
 import os
 from bs4 import BeautifulSoup
 import requests
-
 
 def getlinks(url="https://en.wikipedia.org/wiki/Main_Page", filenum = 0):
 	source = requests.get(url)
@@ -18,23 +16,20 @@ def getlinks(url="https://en.wikipedia.org/wiki/Main_Page", filenum = 0):
 		if (link.get('href','').startswith('/wiki/') and not link.get('href','').startswith('/wiki/Special:')):
 			f.write(link.get('href'))
 			f.write('\n')
-
+	f.close()
 	commandsort = "cat links.txt | sort | uniq -c | sort -n | tail -" + str(numnode) + " | sort -nr > toplinks" + str(filenum) + ".txt"
 	os.system(commandsort)
 	os.system("rm links.txt")
 
 
+# main
 layers = int(input("Enter layers: "))
-
 url = input("Please enter a wikipedia url: ")
 while not url.startswith("https://en.wikipedia.org"):
 	print("Invalid URL")
 	url = input("Enter a new URL: ")
 
-print(url)
-
 count = 0;
-
 while layers>0:
 	getlinks(url, count)
 	linkstr="toplinks"+str(count)+".txt"
